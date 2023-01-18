c715d42e6c8de423498bc4ede5f48a8dd1a2f31a
Vladyslav Haiduk <the.vladhaiduk@gmail.com>
2023-01-18 12:27:17 +0200

    feat: create todos list and render it

--- src/App.svelte
+++ src/App.svelte
@@ -1,1 +1,27 @@
-<h1>Hello, World</h1>
+<script lang="ts">
+  import type { TodoType } from '$root/types';
+
+  let todos: TodoType[] = [
+    {
+      id: crypto.randomUUID(),
+      text: 'Todo 1',
+      completed: false,
+    },
+    {
+      id: crypto.randomUUID(),
+      text: 'Todo 2',
+      completed: false,
+    },
+    {
+      id: crypto.randomUUID(),
+      text: 'Todo 3',
+      completed: false,
+    },
+  ];
+</script>
+
+<ul>
+  {#each todos as todo (todo.id)}
+    <li>{todo.text}</li>
+  {/each}
+</ul>
